90b9b0083f23b2595266ebdc5f11e12441503f62
Walter Landry <wlandry@caltech.edu>
2015-03-10 15:42:49 -0700

    Clarify a variable name

--- src/Table/write_hdf5/write_hdf5_to_file.cxx
+++ src/Table/write_hdf5/write_hdf5_to_file.cxx
@@ -11,11 +11,11 @@ void Tablator::Table::write_hdf5_to_file (H5::H5File &outfile) const
                                             dataspace) };
   for (auto &p : flatten_properties ())
     {
-      H5::StrType fls_type (0, p.second.size ());
+      H5::StrType str_type (0, p.second.size ());
       H5::DataSpace attribute_space (H5S_SCALAR);
       H5::Attribute attribute
-          = table.createAttribute (p.first, fls_type, attribute_space);
-      attribute.write (fls_type, p.second.c_str ());
+          = table.createAttribute (p.first, str_type, attribute_space);
+      attribute.write (str_type, p.second.c_str ());
     }
   table.write (data.data (), compound_type);
 }
