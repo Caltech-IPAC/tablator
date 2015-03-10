699202e2dd96c98dfa403c16b868b5e57c9aa246
Walter Landry <wlandry@caltech.edu>
2015-03-10 15:47:05 -0700

    Make test/convert.sh check good tables

--- test/convert.sh
+++ test/convert.sh
@@ -9,3 +9,13 @@ for table in test/bad_ipac_tables/*; do
     fi
 done
 
+for table in test/*.tbl; do
+    build/tablator $table test.tbl
+    build/tablator $table test.hdf5
+    build/tablator $table test.xml
+    build/tablator $table test.csv
+    build/tablator $table test.tsv
+    rm test.fits
+    build/tablator $table test.fits
+    build/tablator $table test.html
+done
