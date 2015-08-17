34629150980394dfa74d87938b27b91c834d6362
Walter Landry <wlandry@caltech.edu>
2015-08-17 14:15:37 -0700

    Remove outdated comment

--- src/Description.hxx
+++ src/Description.hxx
@@ -9,7 +9,6 @@ class Description
 {
 public:
   std::string value;
-  /// FIXME: Does attributes here needed?
   std::map<std::string, std::string> attributes;
   Description (const std::string &Value) : value (Value) {}
 };
