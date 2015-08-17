4a453f615089f02f89bbeb6bdecbe1760cc3994a
Walter Landry <wlandry@caltech.edu>
2015-08-17 16:18:43 -0700

    Include comments when writing HDF5 files

--- src/Table/write_hdf5/write_hdf5_to_file.cxx
+++ src/Table/write_hdf5/write_hdf5_to_file.cxx
@@ -9,6 +9,22 @@ void tablator::Table::write_hdf5_to_file (H5::H5File &outfile) const
 
   H5::DataSet table{ outfile.createDataSet ("table", compound_type,
                                             dataspace) };
+  if (!comments.empty ())
+    {
+      std::string description;
+      for (auto &c: comments)
+        description+=c + '\n';
+      if (!description.empty ())
+        {
+          description.resize (description.size () - 1);
+          H5::StrType str_type (0, description.size ());
+          H5::DataSpace attribute_space (H5S_SCALAR);
+          H5::Attribute attribute
+            = table.createAttribute ("DESCRIPTION", str_type, attribute_space);
+          attribute.write (str_type, description.c_str ());
+        }
+    }
+
   for (auto &p : flatten_properties ())
     {
       H5::StrType str_type (0, p.second.size ());
