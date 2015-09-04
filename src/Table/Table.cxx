73f3eb60bc7f64529f866499420540294d6eb298
Walter Landry <wlandry@caltech.edu>
2015-09-04 10:26:09 -0700

    Reset the size of Table::compound_type when adding items rather than computing it all beforehand.

--- src/Table/Table.cxx
+++ src/Table/Table.cxx
@@ -2,19 +2,12 @@
 
 tablator::Table::Table (const std::vector<Column> &columns,
                         const std::map<std::string, std::string> &property_map)
-    : compound_type ([&](){
-        size_t sum=(columns.size ()+7)/8;
-        for (auto &c: columns)
-          sum+=c.second.first.first.getSize () * c.second.first.second;
-        return sum;
-      }()),
-      row_size (compound_type.getSize ())
 {
   size_t offset(0);
   const size_t null_flags_size=(columns.size ()+7)/8;
 
-  string_types.emplace_back (0,
-                             null_flags_size);
+  string_types.emplace_back (0, null_flags_size);
+  compound_type.setSize (null_flags_size);
   compound_type.insertMember ("null_bitfield_flags", offset,
                               *string_types.rbegin ());
   offsets.push_back (offset);
@@ -25,10 +18,12 @@ tablator::Table::Table (const std::vector<Column> &columns,
   for (auto &c : columns)
     {
       auto type = c.second.first.first;
+      const size_t member_size=
+        c.second.first.first.getSize () * c.second.first.second;
+      compound_type.setSize (compound_type.getSize () + member_size);
       if (type == H5::PredType::NATIVE_CHAR)
         {
-          string_types.emplace_back (0,
-                                     c.second.first.second);
+          string_types.emplace_back (0, c.second.first.second);
           compound_type.insertMember (c.first, offset,
                                       *string_types.rbegin ());
         }
@@ -37,7 +32,7 @@ tablator::Table::Table (const std::vector<Column> &columns,
           compound_type.insertMember (c.first, offset, type);
         }
       offsets.push_back (offset);
-      offset += c.second.first.first.getSize () * c.second.first.second;
+      offset += member_size;
       fields_properties.emplace_back (c.second.second);
 
       if (type == H5::PredType::NATIVE_UCHAR)
@@ -60,6 +55,8 @@ tablator::Table::Table (const std::vector<Column> &columns,
             + std::to_string (type.getId ()));
     }
   offsets.push_back (offset);
+
+  row_size=compound_type.getSize ();
 
   for (auto &p : property_map)
     properties.emplace_back (p.first, Property (p.second));
