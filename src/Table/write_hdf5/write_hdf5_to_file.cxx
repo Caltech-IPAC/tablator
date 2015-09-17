038812d7b8a466032b1d84e2f497fc55d2f118fa
Walter Landry <wlandry@caltech.edu>
2015-09-17 16:48:28 -0700

    Make write_hdf5 temporarily work by not writing properties

--- src/Table/write_hdf5/write_hdf5_to_file.cxx
+++ src/Table/write_hdf5/write_hdf5_to_file.cxx
@@ -1,4 +1,5 @@
 #include <array>
+#include <sstream>
 
 #include "../../Table.hxx"
 
@@ -25,13 +26,9 @@ void tablator::Table::write_hdf5_to_file (H5::H5File &outfile) const
         }
     }
 
-  for (auto &p : flatten_properties ())
-    {
-      H5::StrType str_type (0, p.second.size ());
-      H5::DataSpace attribute_space (H5S_SCALAR);
-      H5::Attribute attribute
-          = table.createAttribute (p.first, str_type, attribute_space);
-      attribute.write (str_type, p.second.c_str ());
-    }
+  H5::CompType votable (size_t(1));
+  std::stringstream ss;
+  std::vector<H5::StrType> strings;
+  std::vector<H5::CompType> compound_types;
   table.write (data.data (), compound_type);
 }
