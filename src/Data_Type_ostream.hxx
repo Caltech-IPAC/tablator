7500534407ec71474d690ce9b814d91d51be4abc
Walter Landry <wlandry@caltech.edu>
2016-03-19 17:50:21 -0700

    Add a pretty printer for Data_Type

--- src/Data_Type_ostream.hxx
+++ src/Data_Type_ostream.hxx
@@ -1,45 +1,48 @@
 #pragma once
 
+#include "Data_Type.hxx"
+#include <iostream>
+
 namespace tablator
 {
 inline std::ostream &operator<< (std::ostream &os, tablator::Data_Type &type)
 {
   switch (type)
     {
-    case INT8_LE:
+    case Data_Type::INT8_LE:
       os << "INT8_LE";
       break;
-    case UINT8_LE:
+    case Data_Type::UINT8_LE:
       os << "UINT8_LE";
       break;
-    case INT16_LE:
+    case Data_Type::INT16_LE:
       os << "INT16_LE";
       break;
-    case UINT16_LE:
+    case Data_Type::UINT16_LE:
       os << "UINT16_LE";
       break;
-    case INT32_LE:
+    case Data_Type::INT32_LE:
       os << "INT32_LE";
       break;
-    case UINT32_LE:
+    case Data_Type::UINT32_LE:
       os << "UINT32_LE";
       break;
-    case INT64_LE:
+    case Data_Type::INT64_LE:
       os << "INT64_LE";
       break;
-    case UINT64_LE:
+    case Data_Type::UINT64_LE:
       os << "UINT64_LE";
       break;
-    case FLOAT32_LE:
+    case Data_Type::FLOAT32_LE:
       os << "FLOAT32_LE";
       break;
-    case FLOAT64_LE:
+    case Data_Type::FLOAT64_LE:
       os << "FLOAT64_LE";
       break;
-    case STRING:
+    case Data_Type::STRING:
       os << "STRING";
       break;
-    case ARRAY:
+    case Data_Type::ARRAY:
       os << "ARRAY";
       break;
     }
