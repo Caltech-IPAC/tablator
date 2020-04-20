976fbeb886a352960524c4627f705be05b3afd84
Judith Silverman <judith@ipac.caltech.edu>
2020-04-20 07:47:11 -0700

    Port to Debian 11 Part II (changes required by Debian 10)
    read/write_hdf5 functions: Adjusted to accommodate changes to hdf5
      library: replaced H5Location with H5Object in function declarations,
      removed call to strangely unhelpful H5Pset_libver_bounds().
    
      write_hdf5_columns(): renamed to write_hdf5_object(); rearranged
        parameters; replaced "column" in names with "field".
    
    read/write_fits functions: Adjusted to accommodate changes to cfitsio
      library: pulled out helper functions to handle reading and writing
      of labeled_properties.  Now store VOTable-style labels as part of
      kwd_value rather than of kwd_key.
    
    write.cxx: Accommodated changes to boost::property_tree library by
      adding template parameter or replacing "char" with "std::string" in
      calls to
      boost::property_tree::xml_writer_make_settings<std::string>().
    
    tests: Removed newlines before opening braces and brackets in json5
      tables used for back-and-forth conversion tests to accommodate
      change in boost::property_tree::write_json().

--- src/Ipac_Table_Writer.hxx
+++ src/Ipac_Table_Writer.hxx
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <numeric>
 #include <string>
 #include <vector>
 
--- src/Table/read_fits.cxx
+++ src/Table/read_fits.cxx
@@ -21,10 +21,6 @@ namespace {
 // Helper functions for reading keywords
 //====================================================
 
-
-//====================================================
-
-
 // If the present FITS file was created by write_fits() from a table
 // in VOTable format, the table's labeled_properties,
 // trailing_info_lists, and attributes were translated into FITs
@@ -461,6 +457,7 @@ private:
 
 };  // end of Keyword_Reader class
 
+
 //====================================================
 // Helper functions/classes for parsing binary data
 //====================================================
@@ -520,6 +517,7 @@ public:
         column_info_list_.emplace_back(tab_data_type, array_size, got_null,
                                        null_list_idx);
     }
+
 
     //=====================================================
 
@@ -782,7 +780,6 @@ private:
     const Column_Info_Manager &col_info_manager_;
     const std::vector<size_t> &offsets_;
 };
-
 
 }  // namespace
 
