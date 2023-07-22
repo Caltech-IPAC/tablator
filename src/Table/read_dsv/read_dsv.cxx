9e665492efa81cfad5c9f2ecc872ac03b99f77aa
Judith Silverman <judith@ipac.caltech.edu>
2023-07-22 12:38:27 -0700

    Add support for comments in csv/tsv formats
    IRSA-5411

--- src/Table/read_dsv/read_dsv.cxx
+++ src/Table/read_dsv/read_dsv.cxx
@@ -5,8 +5,36 @@ std::list<std::vector<std::string> > parse_dsv(std::istream &input_stream,
                                                const char &delimiter);
 
 void Table::read_dsv(std::istream &input_stream, const Format &format) {
+    // Read and load comments, skipping blank lines and empty comments.
+
+    bool checking_for_comments = true;
+    std::string line;
+
+    while (checking_for_comments) {
+        char first_character = input_stream.get();
+        for (; input_stream && first_character == ' ';
+             first_character = input_stream.get()) {
+        };
+
+        if (first_character == '#') {
+            std::getline(input_stream, line);
+            boost::algorithm::trim(line);
+            if (!line.empty()) {
+                add_comment(line);
+            }
+
+        } else if (first_character != '\n') {
+            checking_for_comments = false;
+            input_stream.putback(first_character);
+        }
+    }
+
+    // Read body of table.
+
     std::list<std::vector<std::string> > dsv(
             parse_dsv(input_stream, format.is_csv() ? ',' : '\t'));
+
+    // Construct main resource element from table data.
 
     std::vector<Column> columns;
     std::vector<size_t> offsets = {0};
