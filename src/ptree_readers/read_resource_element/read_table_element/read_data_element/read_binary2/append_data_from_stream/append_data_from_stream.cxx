cfc400082d3e6224789bae1340912215d9b26c36
Judith Silverman <judith@ipac.caltech.edu>
2025-06-19 10:23:13 -0700

    IRSA-6967: tablator: store row-level size of variable-length arrays I
    Column: Add dynamic_array_flag class member
    ptree_readers: Retire Field_And_Flag helper class

--- src/ptree_readers/read_resource_element/read_table_element/read_data_element/read_binary2/append_data_from_stream/append_data_from_stream.cxx
+++ src/ptree_readers/read_resource_element/read_table_element/read_data_element/read_binary2/append_data_from_stream/append_data_from_stream.cxx
@@ -6,65 +6,78 @@
 #include "../../../../../../Utils/Table_Utils.hxx"
 
 namespace tablator {
-void insert_swapped(const size_t &column_offset, const Data_Type &data_type,
-                    const size_t &array_size, const std::vector<uint8_t> &stream,
-                    const size_t &old_position, Row &row);
-inline void insert_swapped(const size_t &column_offset, const Column &column,
-                           const std::vector<uint8_t> &stream,
-                           const size_t &old_position, Row &row) {
-    return insert_swapped(column_offset, column.get_type(), column.get_array_size(),
-                          stream, old_position, row);
+
+void insert_swapped(Row &row, size_t column_offset, const Data_Type &data_type,
+                    size_t array_size, const std::vector<uint8_t> &stream,
+                    size_t old_position);
+inline void insert_swapped(Row &row, size_t column_offset, const Column &column,
+                           const std::vector<uint8_t> &stream, size_t old_position) {
+    return insert_swapped(row, column_offset, column.get_type(),
+                          column.get_array_size(), stream, old_position);
 }
 
-void ptree_readers::append_data_from_stream(
-        std::vector<uint8_t> &data, const std::vector<Column> &columns,
-        const std::vector<size_t> &offsets, const std::vector<uint8_t> &stream,
-        const std::vector<ptree_readers::Field_And_Flag> &field_flag_pairs,
-        size_t num_rows) {
+void ptree_readers::append_data_from_stream(std::vector<uint8_t> &data,
+                                            const std::vector<Column> &columns,
+                                            const std::vector<size_t> &offsets,
+                                            const std::vector<uint8_t> &stream,
+                                            const std::vector<Field> &fields,
+                                            size_t num_rows) {
     const size_t null_flags_size((columns.size() + 6) / 8);
-    size_t position(0);
+    size_t src_pos(0);
+
     Row row(get_row_size(offsets));
     for (size_t r = 0; r < num_rows; ++r) {
         row.fill_with_zeros();
-        size_t row_offset(position);
-        position += null_flags_size;
+        size_t row_offset(src_pos);
+        src_pos += null_flags_size;
         for (size_t col_idx = 1; col_idx < columns.size(); ++col_idx) {
-            bool dynamic_array_flag =
-                    field_flag_pairs[col_idx].get_dynamic_array_flag();
+            auto column = columns.at(col_idx);
+            auto col_array_size = column.get_array_size();
+            auto col_type = column.get_type();
+
+            bool dynamic_array_flag = fields[col_idx].get_dynamic_array_flag();
             if (is_null_MSB(stream, row_offset, col_idx)) {
-                row.set_null(columns[col_idx].get_type(),
-                             columns[col_idx].get_array_size(), col_idx,
-                             offsets[col_idx], offsets[col_idx + 1]);
-                if (dynamic_array_flag)
-                    position += sizeof(uint32_t);
-                else
-                    position += data_size(columns[col_idx].get_type()) *
-                                columns[col_idx].get_array_size();
-            } else {
+                row.set_null(col_type, col_array_size, col_idx, offsets[col_idx],
+                             offsets[col_idx + 1]);
                 if (dynamic_array_flag) {
-                    auto begin = stream.begin();
-                    std::advance(begin, position);
-                    auto end = stream.begin();
-                    position += sizeof(uint32_t);
-                    std::advance(end, position);
-                    uint32_t dynamic_array_size(0);
-                    boost::spirit::qi::parse(begin, end, boost::spirit::qi::big_dword,
-                                             dynamic_array_size);
-                    insert_swapped(offsets[col_idx], columns[col_idx].get_type(),
-                                   dynamic_array_size, stream, position, row);
-                    position +=
-                            data_size(columns[col_idx].get_type()) * dynamic_array_size;
+				  src_pos += sizeof(uint32_t);
                 } else {
-                    insert_swapped(offsets[col_idx], columns[col_idx], stream, position,
-                                   row);
-                    position += columns[col_idx].get_array_size() *
-                                data_size(columns[col_idx].get_type());
+                    src_pos += data_size(col_type) * col_array_size;
                 }
+            } else if (dynamic_array_flag) {
+                auto begin = stream.begin();
+                std::advance(begin, src_pos);
+                auto end = stream.begin();
+                src_pos += sizeof(uint32_t);
+                std::advance(end, src_pos);
+                uint32_t dynamic_array_size(0);
+
+                // Parse big-ended dynamic_array_size and write it
+                // to <row> (and eventually to <data>)
+                // little-endedly, since <data> is for internal
+                // use only.  We'll swap back if we later write
+                // this table in binary2 format.
+
+                boost::spirit::qi::parse(begin, end, boost::spirit::qi::big_dword,
+                                         dynamic_array_size);
+
+                memcpy(row.get_data().data() + offsets[col_idx], &dynamic_array_size,
+                       sizeof(uint32_t));
+
+                // Now write the array itself, again swapping from
+                // big-ended to little-ended for internal use.
+                insert_swapped(row, offsets[col_idx] + sizeof(uint32_t), col_type,
+                               dynamic_array_size, stream, src_pos);
+                src_pos += data_size(col_type) * dynamic_array_size;
+            } else {
+                insert_swapped(row, offsets[col_idx], column, stream, src_pos);
+                src_pos += col_array_size * data_size(col_type);
             }
-        }
-        if (position <= stream.size()) {
+
+        }  // end loop through columns
+        if (src_pos <= stream.size()) {
             append_row(data, row);
         }
-    }
+    }  // end loop through rows
 }
 }  // namespace tablator
